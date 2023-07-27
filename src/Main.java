71dc0c5b973b10c116d502b7ca31325424981a40
hayate0721 <h08066802495@gmail.com>
2023-07-27 17:32:58 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
