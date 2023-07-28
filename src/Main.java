62a1df4773c792fe2972c432b3a983928c427608
hayate0721 <h08066802495@gmail.com>
2023-07-28 13:36:48 +0900

    Practiced Collections methods

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,40 @@ public class Main {
         int subListIndex = Collections.indexOfSubList(deck, tens);
         System.out.println("sublist index for tens = " + subListIndex);
 
+        deck.sort(sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts));
+        System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        if(Collections.replaceAll(deck, tenOfHearts, tenOfClubs)){
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        }else{
+            System.out.println("No tens of hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " + Collections.frequency(deck, tenOfClubs));
+        System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(deck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card::suit).thenComparing(Card::rank);
+        deck.sort(sortBySuit);
+        Card.printDeck(deck, "Sorted by Suit, Rank", 4);
+
+        List<Card> copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, 2);
+        System.out.println("Rotated " + 2 + ": " + copied);
+
+
+
 
 
 
